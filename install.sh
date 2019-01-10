51a89621ab8fa176fec63d2e81116ada9ee3fca6
David Aguilera <david.aguilera@neliosoftware.com>
2019-01-10 09:03:18 +0100

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,20 +1,59 @@
 #!/bin/bash
 ############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# install.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/Programs/dotfiles
 ############################
 
-########## Variables
+SRC_DIR=~/Programs/dotfiles
 
-dir=~/Programs/bin/dotfiles              # dotfiles directory
 
-##########
+echo "=================="
+echo "BASIC CONFIG FILES"
+echo "=================="
 
-# Create symlinks
-for file in `find . -type f | grep -v "install.sh\|README.md\|LICENSE\|.git"`; do
-	echo $file
-	# echo "Moving any existing dotfiles from ~ to $olddir"
-	# mv ~/.$file ~/dotfiles_old/
-	# echo "Creating symlink to $file in home directory."
-	# ln -s $dir/$file ~/.$file
+for file in `ls -p | grep -v / | grep -v "install.sh\|README.md\|LICENSE"`;
+do
+	echo "Adding ~/.$file..."
+	rm ~/.$file 2>/dev/null >&2
+	ln -s $SRC_DIR/$file ~/.$file
 done
+echo ""
+
+
+echo "========="
+echo "QTERMINAL"
+echo "========="
+
+echo "Adding color schemes..."
+mkdir -p ~/.config/qterminal.org/color-schemes
+ln -s $SRC_DIR/qterminal.org/color-schemes/* ~/.config/qterminal.org/color-schemes/
+echo ""
+
+
+echo "========"
+echo "CINNAMON"
+echo "========"
+
+# APPLETS
+mkdir -p ~/.local/share/cinnamon/applets
+for file in `ls -d local/share/cinnamon/applets/*`;
+do
+	echo "Adding applet `basename $file`..."
+	rm -rf ~/.$file 2>/dev/null >&2
+	ln -s $SRC_DIR/$file ~/.$file
+done
+
+# EXTENSIONS
+mkdir -p ~/.local/share/cinnamon/extensions
+for file in `ls -d local/share/cinnamon/extensions/*`;
+do
+	echo "Adding extension `basename $file`..."
+	rm -rf ~/.$file 2>/dev/null >&2
+	ln -s $SRC_DIR/$file ~/.$file
+done
+echo ""
+
+
+echo "DONE"
+echo "Enjoy your new life, David!"
+
