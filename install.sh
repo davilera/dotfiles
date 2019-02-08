bb1733503de4e0b85b3c373327f015500c774b22
David Aguilera <david.aguilera@neliosoftware.com>
2019-02-08 12:32:58 +0100

    Improved interaction with install script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+ !/bin/bash
 ############################
 # install.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/Programs/dotfiles
 ############################
 
 SRC_DIR=~/Programs/dotfiles
+export DEBIAN_FRONTEND="noninteractive"
 
 
 echo "=================="
@@ -19,8 +20,9 @@ else
 	cd ~/.bash_it 2>&1 >/dev/null
 	git up 2>&1 >/dev/null
 fi
-~/.bash_it/install.sh --no-modify-config --silent
+~/.bash_it/install.sh --no-modify-config --silent >/dev/null 2>&1
 
+cd $SRC_DIR
 for file in `ls -p | grep -v / | grep -v "install.sh\|README.md\|LICENSE"`;
 do
 	echo "Adding ~/.$file..."
@@ -35,6 +37,7 @@ echo "QTERMINAL"
 echo "========="
 
 mkdir -p ~/.config/qterminal.org/color-schemes
+cd $SRC_DIR
 for file in `ls -d config/qterminal.org/color-schemes/*`;
 do
 	echo "Adding colorscheme `basename $file`..."
@@ -50,6 +53,7 @@ echo "========"
 
 # APPLETS
 mkdir -p ~/.local/share/cinnamon/applets
+cd $SRC_DIR
 for file in `ls -d local/share/cinnamon/applets/*`;
 do
 	echo "Adding applet `basename $file`..."
@@ -59,6 +63,7 @@ done
 
 # EXTENSIONS
 mkdir -p ~/.local/share/cinnamon/extensions
+cd $SRC_DIR
 for file in `ls -d local/share/cinnamon/extensions/*`;
 do
 	echo "Adding extension `basename $file`..."
@@ -85,18 +90,35 @@ ln -s $SRC_DIR/Programs/bin ~/Programs/
 echo ""
 
 
-echo "========================"
-echo "OPTIONAL SYSTEM PAKCAGES"
-echo "========================"
+echo ""
+echo "======"
+echo "SYSTEM"
+echo "======"
 
-echo -n "Do you want to install system packages? (y/N) "
-read answer
-if [ "$answer" == "y" ];
-then
-	echo "Installing development packages..."
-	sudo apt install fasd tree meld docker.io docker-compose vim ruby subversion composer php7.2-xml inkscape gimp
-	sudo update-alternatives --set editor /usr/bin/vim.basic
-fi
+echo "Let's perform some system changes..."
+sudo ls >/dev/null 2>&1
+
+echo ""
+echo "Installing dev packages..."
+sudo apt-get -qq install fasd tree meld docker.io docker-compose vim ruby subversion composer php7.2-xml
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
+echo "Installing utilities..."
+sudo apt-get -qq install inkscape gimp filezilla
+
+echo "Updating apt packages..."
+sudo apt-get -qq update
+echo "Upgrading system..."
+sudo apt-get -qq upgrade
+echo "Cleaning unnecessary packages..."
+sudo apt-get -qq autoremove
+sudo apt-get -qq autoclean
+
+
+echo ""
+echo "==="
+echo "VIM"
+echo "==="
 
 echo ""
 echo -n "Do you want to install vim packages? (y/N) "
@@ -105,7 +127,7 @@ if [ "$answer" == "y" ];
 then
 
 	# YouCompleteMe...
-	sudo apt install build-essential cmake python2.7-dev python3-dev
+	sudo apt-get -qq install build-essential cmake python2.7-dev python3-dev
 	cd ~/.vim/bundle
 	if [ -d YouCompleteMe ];
 	then
@@ -116,29 +138,11 @@ then
 	git pull
 	git submodule update --init --recursive
 	./install.sh
-	sudo apt remove build-essential cmake python2.7-dev python3-dev
+	sudo apt-get -qq remove build-essential cmake python2.7-dev python3-dev
 
 	# Other packages
 	vim +PluginInstall +qall
 
-fi
-
-echo ""
-echo -n "Do you want to update the system? (y/N) "
-read answer
-if [ "$answer" == "y" ];
-then
-	sudo apt update
-	sudo apt upgrade
-fi
-
-echo ""
-echo -n "Do you want to remove unused packages and autoclean the system? (y/N) "
-read answer
-if [ "$answer" == "y" ];
-then
-	sudo apt autoremove
-	sudo apt autoclean
 fi
 
 echo ""
