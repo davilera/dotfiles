bbff312faa8de85048efe9dfa7ab149ff5d8d2be
David Aguilera <david.aguilera@neliosoftware.com>
2023-04-03 11:54:24 +0200

    Add eslint to diagnostics

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -4,17 +4,20 @@ if not status_ok then
 end
 
 local completion = null_ls.builtins.completion
+local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
 	sources = {
 		completion.spell,
+		diagnostics.eslint_d,
+		formatting.elm_format,
+		formatting.eslint_d,
 		formatting.phpcbf.with({
 			command = './vendor/bin/phpcbf',
 			args = { '-q', '--stdin-path=$FILENAME', '--standard=phpcs.ruleset.xml', '-' },
 		}),
 		formatting.prettier,
 		formatting.stylua,
-		formatting.elm_format,
 	},
 })
