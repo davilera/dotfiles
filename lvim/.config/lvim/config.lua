36fec75d8f8d98081ae531da75848b2b822ec092
David Aguilera <david.aguilera@neliosoftware.com>
2023-06-26 15:23:05 +0200

    Enable custom plugins

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -17,33 +17,33 @@ lvim.builtin.indentlines.active = false
 lvim.builtin.project.manual_mode = true
 
 local options = {
-  backupdir = '/tmp',     -- location of swap files
-  clipboard = '',         -- disable integration with system clipboard
-  cmdheight = 2,          -- more space in neovim’s command line for displaying messages
-  cursorline = true,      -- highlight current line
-  cursorlineopt = 'both', -- highlight current line
-  encoding = 'utf-8',     -- fix encoding
-  hidden = true,          -- prefer hiding over unloading buffers
-  hlsearch = true,        -- highlight search
-  mouse = '',             -- disable mouse
-  number = true,          -- show line numbers (current line only when combined with relative numbers)
-  path = '.,,',           -- search relative to current file + directory
-  relativenumber = true,  -- show relative numbers
-  scrolloff = 8,          -- keep some context around current line
-  sidescrolloff = 8,      -- keep some context around current line
-  shiftwidth = 2,         -- indentation defaults (<< / >> / == / auto)
-  smartindent = true,     -- try to guess current indentation level
-  splitbelow = true,      -- prefer new horizontal split below
-  splitright = true,      -- prefer new vertical split to the right
-  tabstop = 2,            -- tab characters take two spaces only
-  title = false,          -- disable title string
-  updatetime = 300,       -- faster autocomplete
-  undolevels = 100,       -- number of undo levels
-  wrap = false,           -- wrap long lines
+	backupdir = '/tmp',    -- location of swap files
+	clipboard = '',        -- disable integration with system clipboard
+	cmdheight = 2,         -- more space in neovim’s command line for displaying messages
+	cursorline = true,     -- highlight current line
+	cursorlineopt = 'both', -- highlight current line
+	encoding = 'utf-8',    -- fix encoding
+	hidden = true,         -- prefer hiding over unloading buffers
+	hlsearch = true,       -- highlight search
+	mouse = '',            -- disable mouse
+	number = true,         -- show line numbers (current line only when combined with relative numbers)
+	path = '.,,',          -- search relative to current file + directory
+	relativenumber = true, -- show relative numbers
+	scrolloff = 8,         -- keep some context around current line
+	sidescrolloff = 8,     -- keep some context around current line
+	shiftwidth = 2,        -- indentation defaults (<< / >> / == / auto)
+	smartindent = true,    -- try to guess current indentation level
+	splitbelow = true,     -- prefer new horizontal split below
+	splitright = true,     -- prefer new vertical split to the right
+	tabstop = 2,           -- tab characters take two spaces only
+	title = false,         -- disable title string
+	updatetime = 300,      -- faster autocomplete
+	undolevels = 100,      -- number of undo levels
+	wrap = false,          -- wrap long lines
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 
@@ -51,51 +51,57 @@ end
 --   CUSTOM PLUGINS
 -- =======================================================
 
-lvim.init = {
-  -- Tab/space behavior
-  {
-    'FotiadisM/tabset.nvim',
-    setup = {
-      defaults = {
-        tabwidth = 2,
-        expandtab = false,
-      },
-      languages = {
-        elm = {
-          tabwidth = 4,
-          expandtab = true,
-        },
-        haskell = {
-          tabwidth = 4,
-          expandtab = true,
-        },
-        markdown = {
-          tabwidth = 2,
-          expandtab = true,
-        },
-        yaml = {
-          tabwidth = 4,
-          expandtab = true,
-        },
-      },
-    },
-  },
+lvim.plugins = {
+	-- Tab/space behavior
+	{
+		'FotiadisM/tabset.nvim',
+		config = {
+			defaults = {
+				tabwidth = 2,
+				expandtab = false,
+			},
+			languages = {
+				elm = {
+					tabwidth = 4,
+					expandtab = true,
+				},
+				haskell = {
+					tabwidth = 4,
+					expandtab = true,
+				},
+				markdown = {
+					tabwidth = 2,
+					expandtab = true,
+				},
+				yaml = {
+					tabwidth = 4,
+					expandtab = true,
+				},
+			},
+		},
+	},
 
-  -- Colors preview
-  { 'brenoprata10/nvim-highlight-colors' },
+	-- Colors preview
+	{
+		'brenoprata10/nvim-highlight-colors',
+		config = {
+			render = 'background',
+			enabled_named_colors = true,
+		},
+	},
 
-  -- Show marks
-  {
-    'chentoast/marks.nvim',
-    config = {
-      default_mappings = true,
-      signs = true,
-      mappings = {},
-    }
-  },
+	-- Show marks
+	{
+		'chentoast/marks.nvim',
+		config = {
+			default_mappings = true,
+			signs = true,
+			mappings = {},
+		}
+	},
 
-  -- Text case conversions
-  { 'johmsalas/text-case.nvim' },
+	-- Text case conversions
+	{ 'johmsalas/text-case.nvim' },
 }
 
 -- Remove marks when opening file
@@ -130,17 +136,17 @@ lvim.builtin.which_key.mappings['b']['W'] = nil
 ---- Close app/tab -------------
 --------------------------------
 local function closeTabAndSplit()
-  vim.cmd(':BufferKill');
-  local files = #vim.fn.getbufinfo({ buflisted = 1 })
-  local splits = #vim.api.nvim_tabpage_list_wins(0);
-  local dirty = 1 == vim.fn.getbufinfo()[vim.fn.bufnr()].changed
-  if files == 1 then
-    if not dirty then
-      vim.cmd(':q')
-    end
-  elseif not dirty and splits > 1 then
-    vim.cmd(':q')
-  end
+	vim.cmd(':BufferKill');
+	local files = #vim.fn.getbufinfo({ buflisted = 1 })
+	local splits = #vim.api.nvim_tabpage_list_wins(0);
+	local dirty = 1 == vim.fn.getbufinfo()[vim.fn.bufnr()].changed
+	if files == 1 then
+		if not dirty then
+			vim.cmd(':q')
+		end
+	elseif not dirty and splits > 1 then
+		vim.cmd(':q')
+	end
 end
 
 lvim.builtin.which_key.mappings['Q'] = { ':confirm qall<cr>', 'Force quit' }
@@ -152,26 +158,26 @@ lvim.builtin.which_key.mappings['F'] = lvim.builtin.which_key.mappings['b']['W']
 ---- Configure smart case ------
 --------------------------------
 lvim.builtin.which_key.mappings['c'] = {
-  name = 'Case',
-  ['.'] = { "<cmd>lua require('textcase').current_word('to_dot_case')<cr>", 'Dot case' },
-  ['-'] = { "<cmd>lua require('textcase').current_word('to_dash_case')<cr>", 'Dash case' },
-  ['_'] = { "<cmd>lua require('textcase').current_word('to_snake_case')<cr>", 'Snake case' },
-  c = { "<cmd>lua require('textcase').current_word('to_camel_case')<cr>", 'Camel case' },
-  k = { "<cmd>lua require('textcase').current_word('to_constant_case')<cr>", 'Constant case' },
-  l = { "<cmd>lua require('textcase').current_word('to_lower_case')<cr>", 'Lower case' },
-  p = { "<cmd>lua require('textcase').current_word('to_pascal_case')<cr>", 'Pascal case' },
-  u = { "<cmd>lua require('textcase').current_word('to_upper_case')<cr>", 'Upper case' },
+	name = 'Case',
+	['.'] = { "<cmd>lua require('textcase').current_word('to_dot_case')<cr>", 'Dot case' },
+	['-'] = { "<cmd>lua require('textcase').current_word('to_dash_case')<cr>", 'Dash case' },
+	['_'] = { "<cmd>lua require('textcase').current_word('to_snake_case')<cr>", 'Snake case' },
+	c = { "<cmd>lua require('textcase').current_word('to_camel_case')<cr>", 'Camel case' },
+	k = { "<cmd>lua require('textcase').current_word('to_constant_case')<cr>", 'Constant case' },
+	l = { "<cmd>lua require('textcase').current_word('to_lower_case')<cr>", 'Lower case' },
+	p = { "<cmd>lua require('textcase').current_word('to_pascal_case')<cr>", 'Pascal case' },
+	u = { "<cmd>lua require('textcase').current_word('to_upper_case')<cr>", 'Upper case' },
 }
 lvim.builtin.which_key.vmappings['c'] = {
-  name = 'Case',
-  ['.'] = { "<cmd>lua require('textcase').operator('to_dot_case')<cr>", 'Dot case' },
-  ['-'] = { "<cmd>lua require('textcase').operator('to_dash_case')<cr>", 'Dash case' },
-  ['_'] = { "<cmd>lua require('textcase').operator('to_snake_case')<cr>", 'Snake case' },
-  c = { "<cmd>lua require('textcase').operator('to_camel_case')<cr>", 'Camel case' },
-  k = { "<cmd>lua require('textcase').operator('to_constant_case')<cr>", 'Constant case' },
-  l = { "<cmd>lua require('textcase').operator('to_lower_case')<cr>", 'Lower case' },
-  p = { "<cmd>lua require('textcase').operator('to_pascal_case')<cr>", 'Pascal case' },
-  u = { "<cmd>lua require('textcase').operator('to_upper_case')<cr>", 'Upper case' },
+	name = 'Case',
+	['.'] = { "<cmd>lua require('textcase').operator('to_dot_case')<cr>", 'Dot case' },
+	['-'] = { "<cmd>lua require('textcase').operator('to_dash_case')<cr>", 'Dash case' },
+	['_'] = { "<cmd>lua require('textcase').operator('to_snake_case')<cr>", 'Snake case' },
+	c = { "<cmd>lua require('textcase').operator('to_camel_case')<cr>", 'Camel case' },
+	k = { "<cmd>lua require('textcase').operator('to_constant_case')<cr>", 'Constant case' },
+	l = { "<cmd>lua require('textcase').operator('to_lower_case')<cr>", 'Lower case' },
+	p = { "<cmd>lua require('textcase').operator('to_pascal_case')<cr>", 'Pascal case' },
+	u = { "<cmd>lua require('textcase').operator('to_upper_case')<cr>", 'Upper case' },
 }
 
 --------------------------------
@@ -179,12 +185,12 @@ lvim.builtin.which_key.vmappings['c'] = {
 --------------------------------
 lvim.builtin.which_key.mappings['l']['M'] = lvim.builtin.which_key.mappings['l']['I']
 lvim.builtin.which_key.mappings['l']['L'] = {
-  lvim.builtin.which_key.mappings['l']['i'][1],
-  'LSP Info'
+	lvim.builtin.which_key.mappings['l']['i'][1],
+	'LSP Info'
 }
 lvim.builtin.which_key.mappings['l']['I'] = nil
 lvim.builtin.which_key.mappings['l']['i'] = {
-  '<cmd>lua vim.lsp.buf.hover()<cr>', 'Information'
+	'<cmd>lua vim.lsp.buf.hover()<cr>', 'Information'
 }
 
 
@@ -194,26 +200,26 @@ lvim.builtin.which_key.mappings['l']['i'] = {
 
 local formatters = require 'lvim.lsp.null-ls.formatters'
 formatters.setup({
-  {
-    name = 'phpcbf',
-    command = './vendor/bin/phpcbf',
-    args = { '-q', '--stdin-path=$FILENAME', '--standard=phpcs.ruleset.xml', '-' },
-  },
-  {
-    name = 'prettier',
-    filetypes = { 'javascript', 'typescript', 'typescriptreact' },
-  }
+	{
+		name = 'phpcbf',
+		command = './vendor/bin/phpcbf',
+		args = { '-q', '--stdin-path=$FILENAME', '--standard=phpcs.ruleset.xml', '-' },
+	},
+	{
+		name = 'prettier',
+		filetypes = { 'javascript', 'typescript', 'typescriptreact' },
+	}
 })
 
 
 local linters = require 'lvim.lsp.null-ls.linters'
 linters.setup({
-  {
-    name = 'prettier',
-    filetypes = { 'javascript', 'typescript', 'typescriptreact' },
-  },
-  {
-    name = 'eslint_d',
-    filetypes = { 'javascript', 'typescript', 'typescriptreact' },
-  },
+	{
+		name = 'prettier',
+		filetypes = { 'javascript', 'typescript', 'typescriptreact' },
+	},
+	{
+		name = 'eslint_d',
+		filetypes = { 'javascript', 'typescript', 'typescriptreact' },
+	},
 })
