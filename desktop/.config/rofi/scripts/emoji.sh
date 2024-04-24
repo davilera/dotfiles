e78109378a47d4fd6990dd0cbb420c978cc47a51
David Aguilera <david.aguilera@neliosoftware.com>
2024-04-24 18:42:20 +0200

    Add emojis to rofi

---
+++ desktop/.config/rofi/scripts/emoji.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install this:
+# https://github.com/Mange/rofi-emoji
+
+DIR="$HOME/.config/rofi"
+
+rofi -no-config -no-lazy-grab -show emoji -modi emoji -theme ~/.config/rofi/emoji.rasi
