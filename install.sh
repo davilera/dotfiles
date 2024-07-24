1f08358c24e001181061d7cbc2ce23deb2ca5358
David Aguilera <david.aguilera@neliosoftware.com>
2024-07-24 13:06:10 +0200

    Fix script

--- install.sh
+++ install.sh
@@ -168,7 +168,7 @@ LV_BRANCH=$version bash <(curl -s https://raw.githubusercontent.com/LunarVim/Lun
 rm -rf ~/.local/share/lunarvim.old  ~/.cache/lvim.old
 
 echo "Installing i3 dependencies…"
-sudo echo "deb [arch=amd64] http://debian.sur5r.net/i3/ jammy universe" > /etc/apt/sources.list.d/sur5r-i3.list
+echo "deb [arch=amd64] http://debian.sur5r.net/i3/ jammy universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list >/dev/null
 /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb /tmp/keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
 sudo apt install /tmp/keyring.deb
 rm /tmp/keyring.deb
