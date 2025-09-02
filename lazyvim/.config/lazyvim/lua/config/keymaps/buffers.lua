6b1621eb395109f04848d1126ea2d37663503acf
David Aguilera <david.aguilera@neliosoftware.com>
2025-09-02 08:16:17 +0200

    Improve buffer swapping

--- lazyvim/.config/lazyvim/lua/config/keymaps/buffers.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps/buffers.lua
@@ -1,8 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 local map = vim.keymap.set
 
-map("n", "<C-p>", ":bprev<CR>", { desc = "Go to Previews Buffer", remap = true })
-map("n", "<C-n>", ":bnext<CR>", { desc = "Go to Next Buffer", remap = true })
+map("n", "<C-p>", "<cmd>bprev<cr><esc>", { desc = "Go to Previews Buffer", remap = true })
+map("n", "<C-n>", "<cmd>bnext<cr><esc>", { desc = "Go to Next Buffer", remap = true })
