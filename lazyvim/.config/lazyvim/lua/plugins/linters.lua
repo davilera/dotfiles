aa04484f46e98e251737c6c1b4e525528849acfd
David Aguilera <david.aguilera@neliosoftware.com>
2025-09-08 09:36:03 +0200

    Fix phpcs path in lazyvim

--- lazyvim/.config/lazyvim/lua/plugins/linters.lua
+++ lazyvim/.config/lazyvim/lua/plugins/linters.lua
@@ -1,6 +1,11 @@
 return {
   "mfussenegger/nvim-lint",
   opts = {
+    linters = {
+      phpcs = {
+        cmd = vim.fn.exepath("phpcs"),
+      },
+    },
     linters_by_ft = {
       css = { "stylelint" },
       scss = { "stylelint" },
