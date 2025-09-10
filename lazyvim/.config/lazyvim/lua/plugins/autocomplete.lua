92e3e278cf48a80660d590cc9994e9a78bf2bd55
David Aguilera <david.aguilera@neliosoftware.com>
2025-09-10 15:58:12 +0200

    Tweak lazyvim plugins

---
+++ lazyvim/.config/lazyvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter",
+    },
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = false,
+        },
+      },
+    },
+  },
+}
---
+++ lazyvim/.config/lazyvim/lua/plugins/disable-autopairs.lua
@@ -0,0 +1,4 @@
+return {
+  "mini.pairs",
+  enabled = false,
+}
