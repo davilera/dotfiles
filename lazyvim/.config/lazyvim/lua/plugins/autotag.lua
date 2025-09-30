b2ef29c494e288970e17a7ddb90685d193fa5d8c
David Aguilera <david.aguilera@neliosoftware.com>
2025-09-30 15:05:46 +0200

    Replace autotag in vim to fix issue with greater than character

---
+++ lazyvim/.config/lazyvim/lua/plugins/autotag.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    enabled = false,
+  },
+  {
+    "tronikelis/ts-autotag.nvim",
+    opts = {
+      auto_rename = {
+        enabled = true,
+      },
+    },
+  },
+}
