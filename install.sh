1b53dc1637a16be9e88033d4defe47d3c8454ec3
David Aguilera <david.aguilera@neliosoftware.com>
2025-10-01 21:23:33 +0200

    Install openssh as well

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "SYSTEM"
 echo "======"
 
 echo "Installing dev packages…"
-sudo pacman -S --noconfirm stow zoxide tree meld jq ruby subversion composer curl python3 python-pip go cargo luarocks prettyping lsd
+sudo pacman -S --noconfirm stow zoxide tree meld jq ruby subversion composer curl python3 python-pip go cargo luarocks prettyping lsd openssh
 
 echo "Installing utilities…"
 sudo pacman -S --noconfirm filezilla btop imagemagick poedit hunspell-es_any aspell-ca aspell-es the_silver_searcher difftastic aws-cli-v2 perl-image-exiftool
