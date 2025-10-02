42fec53ffa1b60e9b975009d645aca42cae3bff2
David Aguilera <david.aguilera@neliosoftware.com>
2025-10-02 11:37:11 +0200

    Beautify install.sh

--- install.sh
+++ install.sh
@@ -3,21 +3,44 @@
 cd "$(dirname "$0")"
 SRC_DIR=$(pwd)
 
-echo "Updating pacman packages…"
+title() {
+  local text
+  text="  $(echo "$1" | xargs)  "
+  local spaces
+  spaces="$(echo "$text" | tr '[:print:]' ' ')"
+  echo -e "\e[48;5;25m"
+  echo "$spaces"
+  echo "$text"
+  echo "$spaces"
+  echo -e "\e[0m"
+}
+
+subtitle() {
+  local text
+  text="$(echo "$1" | xargs)"
+  echo -e "\e[96m"
+  echo -n "► $text"
+  echo -e "\e[0m"
+}
+
+############
+title "INIT"
+############
+
+subtitle "Updating pacman packages…"
 sudo pacman -Syu
 
-echo "Installing basic stuff…"
+subtitle "Installing basic stuff…"
 sudo pacman -S --noconfirm curl git vim kitty thefuck hplip
 
-echo ""
-echo "======"
-echo "SYSTEM"
-echo "======"
+##############
+title "SYSTEM"
+##############
 
-echo "Installing dev packages…"
+subtitle "Installing dev packages…"
 sudo pacman -S --noconfirm stow zoxide tree meld jq ruby subversion composer curl python3 python-pip go cargo luarocks prettyping lsd
 
-echo "Installing utilities…"
+subtitle "Installing utilities…"
 sudo pacman -S --noconfirm filezilla btop imagemagick poedit hunspell-es_any aspell-ca aspell-es the_silver_searcher difftastic aws-cli-v2 perl-image-exiftool
 yay -S --noconfirm hunspell-ca
 
@@ -28,94 +51,92 @@ python -m markdown $@
 EOF
 sudo chmod a+x /usr/local/bin/markdown
 
-echo "Installing openssh…"
+subtitle "Installing openssh…"
 sudo pacman -S --noconfirm openssh
 sudo sed -i "s/^# *Port 22/Port 22/" /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
-echo "Installing nvm, npm, node, bun…"
+subtitle "Installing nvm, npm, node, bun…"
 sudo pacman -S --noconfirm nvm npm bun-bin
 source /usr/share/nvm/init-nvm.sh
 nvm install 20 2>/dev/null
 nvm use 20 2>/dev/null
 npm install -g yarn >/dev/null 2>&1
 
-echo "Installing elm…"
+subtitle "Installing elm…"
 npm install -g elm elm-test elm-format elm-oracle >/dev/null 2>&1
 
-echo "Installing nvim helpers…"
+subtitle "Installing nvim helpers…"
 # TODO. Review this.
-echo " » prettier"
-npm install -g prettier@npm:wp-prettier@latest 2>&1
-echo " » elm tooling"
+echo -ne " » prettier\r"
+npm install -g prettier@npm:wp-prettier@latest >/dev/null 2>&1
+echo -ne " » elm tooling\r"
 npm install -g @elm-tooling/elm-language-server >/dev/null 2>&1
-echo " » emmet"
+echo -ne " » emmet\r"
 npm install -g emmet-ls >/dev/null 2>&1
-echo " » intelephense"
+echo -ne " » intelephense\r"
 npm install -g intelephense >/dev/null 2>&1
-echo " » vscode landservers"
+echo -ne " » vscode landservers\r"
 npm install -g vscode-langservers-extracted >/dev/null 2>&1
-echo " » eslint"
+echo -ne " » eslint\r"
 npm install -g @wordpress/eslint-plugin @typescript-eslint/eslint-plugin @typescript-eslint/parser >/dev/null 2>&1
 
-echo "Installing script helpers…"
+subtitle "Installing script helpers…"
 npm install -g glob lodash path >/dev/null 2>&1
 
-echo "Installing composer deps…"
+subtitle "Installing composer deps…"
 cd "$SRC_DIR" 2>/dev/null
 stow --no-folding composer >/dev/null 2>&1
 composer global install >/dev/null 2>&1
 cd - 2>/dev/null
 
-echo "Installing apps…"
+subtitle "Installing apps…"
 sudo pacman -S --noconfirm inkscape gimp
 yay -S --noconfirm zoom
 
-echo "Configuring inkscape…"
+subtitle "Configuring inkscape…"
 mkdir -p ~/.config/inkscape/extensions >/dev/null 2>&1
 cd ~/.config/inkscape/extensions >/dev/null 2>&1
 wget "https://raw.githubusercontent.com/Klowner/inkscape-applytransforms/master/applytransform.py" >/dev/null 2>&1
 wget "https://raw.githubusercontent.com/Klowner/inkscape-applytransforms/master/applytransform.inx" >/dev/null 2>&1
 cd - >/dev/null 2>&1
 
-echo "Installing Firefox…"
+subtitle "Installing Firefox…"
 sudo pacman -Syy
 sudo pacman -S --noconfirm firefox firefoxpwa
 
-echo "Installing firefoxpwa’s runtime…"
+subtitle "Installing firefoxpwa’s runtime…"
 firefoxpwa runtime install
 
-echo "Installing WhatsApp webapp…"
+subtitle "Installing WhatsApp webapp…"
 if [ "$(firefoxpwa profile list | grep -c whatsapp.web)" -eq 0 ]; then
   profile="$(firefoxpwa profile create | grep "Profile created" | sed -e "s/.*Profile created: //")"
   firefoxpwa site install https://web.whatsapp.com/data/manifest.json --profile "$profile"
 fi
 
-echo "Installing Lando…"
+subtitle "Installing Lando…"
 /bin/bash -c "$(curl -fsSL https://get.lando.dev/setup-lando.sh)"
 eval "$(/home/david/.lando/bin/lando shellenv)"
 
-echo ""
-echo "====================="
-echo "UTILITIES FROM GITHUB"
-echo "====================="
+#############################
+title "UTILITIES FROM GITHUB"
+#############################
 
 # TODO. View how to integrate with Omarchy.
 # echo "Installing greenclip…"
 # sudo pacman -S --noconfirm rofi-greenclip
 
 # TODO. Review how to use colorscheme from Omarchy theme.
-echo "Configuring bat…"
+subtitle "Configuring bat…"
 mkdir -p "$(bat --config-dir)/themes" >/dev/null 2>&1
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme >/dev/null 2>&1
 bat cache --build >/dev/null 2>&1
 
-echo ""
-echo "============="
-echo "INIT DOTFILES"
-echo "============="
+#####################
+title "INIT DOTFILES"
+#####################
 
-echo "Stowing dotfiles…"
+subtitle "Stowing dotfiles…"
 mkdir -p ~/.local/bin 2>/dev/null
 cd "$SRC_DIR" 2>/dev/null
 rm -rf ~/.git ~/.bash* ~/.vim 2>/dev/null
@@ -130,10 +151,9 @@ rm -rf ~/.config/nvim 2>/dev/null
 stow nvim
 cd - 2>/dev/null
 
-echo ""
-echo "======================="
-echo "LOAD ADDITIONAL CONFIGS"
-echo "======================="
+#############################S"
+title "LOAD ADDITIONAL CONFIGS"
+#############################S"
 
 if [ "$(~/.local/share/omarchy/bin/omarchy-theme-current)" != "Catppuccin" ]; then
   yes n | ~/.local/share/omarchy/bin/omarchy-theme-set catppuccin
@@ -142,8 +162,5 @@ yes n | ~/.local/share/omarchy/bin/omarchy-install-terminal kitty
 dconf load /org/gnome/meld/ <"${SRC_DIR}/dconf/meld.ini"
 xdg-settings set default-web-browser firefox.desktop
 
-echo ""
-echo "DONE"
-echo "Enjoy your new life, David!"
-
+echo -e "\e[32mDone! Enjoy your new life, David.\e[0m"
 gum confirm "Relaunch Hyprland to use new settings?" && uwsm stop
