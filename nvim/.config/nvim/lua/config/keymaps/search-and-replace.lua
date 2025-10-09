3fb7e162db345ea8f3cecd126f757737052bff10
David Aguilera <david.aguilera@neliosoftware.com>
2025-10-09 10:37:11 +0200

    Tweak nvim to paste over selection while keeping register

--- nvim/.config/nvim/lua/config/keymaps/search-and-replace.lua
+++ nvim/.config/nvim/lua/config/keymaps/search-and-replace.lua
@@ -1,3 +1,6 @@
 local map = vim.keymap.set
 
 map("v", "<leader>r", '"hy:%s/<C-r>h//gc<left><left><left>', { desc = "Replace" })
+
+-- Paste, reselect, then yank the pasted text to restore unnamed register
+map("x", "p", 'pgv"0y')
